eebda5eeb2a9bd88b0a2b5747371c42f0b417ba5
Pierre Soille <Pierre.Soille@jrc.ec.europa.eu>
2016-09-05 13:40:00 +0200

    removed banner.h since it is created by Makefile

--- core/c/banner.h
+++
@@ -1,2 +0,0 @@
-#define MYBANNER "File created using programs by Pierre Soille et al.  Version as of gio 18 ago 2016, 16.45.35, CEST"
-#define MIA_BANNER "MIALib version as of gio 18 ago 2016, 16.45.35, CEST"
