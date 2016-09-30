b4dc752235fe9adb6b52b4c3df70c7e76c352cb8
Pierre Soille <Pierre.Soille@jrc.ec.europa.eu>
2016-09-30 08:14:32 +0200

    Added missing setup.py file.

---
+++ swig/python/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG mialib
+"""
+
+# see also https://docs.python.org/2/distutils/setupscript.html
+# see also https://docs.python.org/2/tutorial/modules.html
+
+from distutils.core import setup, Extension
+
+#from setuptools import setup, find_packages
+
+mialib_module = Extension('mialib/_mialib', ['mialib.i'],
+                          swig_opts = ['-v', '-Wall',  '-I../include', '-outdir', './packages',
+                                       '-I../include/python',
+                                       '-I/usr/local/lib/python2.7/dist-packages/numpy/core/include',
+                                       '-I/usr/local/lib/python2.7',
+                                       '-I../../core/c',
+                                       '-DDOMINIK'],
+                          libraries = ['gdal', 'tiff', 'mialib_python'],
+                          include_dirs= ['/usr/local/lib/python2.7/dist-packages/numpy/core/include', '/usr/local/lib/python2.7', '../../core/c/'],
+                          define_macros = [('DOMINIK', None)],)
+
+
+# mialib_module = Extension('_mialib',
+#                           sources=['./extensions/mialib_wrap.c'],
+#                           include_dirs= ['/usr/local/lib/python2.7/dist-packages/numpy/core/include', '/usr/local/lib/python2.7', '../../core/c/'],
+#                           define_macros = [('DOMINIK', None)],
+#                           )
+
+setup (name = 'mialib',
+       version = '0.1',
+       author      = "Pierre Soille",
+       author_email      = "Pierre.Soille@jrc.ec.europa.eu",
+       copyright = "(c) European Commission",
+       license = '(c) European Commission. Exact licence to be defined',
+       description = """Python interface to mialib thanks to SWIG""",
+       long_description = """Python interface to mialib thanks to SWIG: long description""",
+       url = "http://jeodpp.ec.europa.eu",
+       ext_modules = [mialib_module],
+       py_modules = ["mialib/mialib"],
+       package_dir = {'' : 'packages'},
+       packages=['mialib', 'mialib/format', 'mialib/geodesy', 'mialib/geometry', 'mialib/io', 'mialib/pointop', 'mialib/visu'],
+       #data_files=[('bitmaps', ['bm/b1.gif', 'bm/b2.gif'])],
+       #package_data={'mypkg': ['data/*.dat']},
+       build_dir = {'' : '../build'}
+       )
