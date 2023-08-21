9a361b766e5b851904fe0fce0c8811e785d6d1d4
Pieter Kempeneers <pieter.kempeneers@ec.europa.eu>
2023-08-21 16:16:22 +0200

    set ENABLE_OPENMP_FFTW ON

--- cmake/modules/FindFFTW.cmake
+++ cmake/modules/FindFFTW.cmake
@@ -140,8 +140,11 @@ if(FFTWL_LIB)
   set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
 endif()
 
-# we will set ENABLE_OPENMP_FFTW from CMakelLists in miallib
+# we need ENABLE_OPENMP_FFTW
+set (ENABLE_OPENMP_FFTW ON)
+message(STATUS "ENABLE_OPENMP_FFTW is ${ENABLE_OPENMP_FFTW}")
 # option (ENABLE_OPENMP_FFTW "Enable usage of OpenMP in FFTW, if possible" ${ENABLE_OPENMP})
+# set (ENABLE_OPENMP_FFTW "Enable usage of OpenMP in FFTW, if possible" ${ENABLE_OPENMP})
 
 if (FFTW_LIB_OMP AND ENABLE_OPENMP_FFTW)
   set(FFTW_LIBRARIES ${FFTW_LIB_OMP} ${FFTW_LIBRARIES})
